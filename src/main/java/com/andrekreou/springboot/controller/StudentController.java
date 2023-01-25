8b6021aaf0ef428094daab5d0aa47a6ddb0d4773
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2023-01-25 13:52:04 +0200

    Created a Delete Request method with PathVariable annotation

--- src/main/java/com/andrekreou/springboot/controller/StudentController.java
+++ src/main/java/com/andrekreou/springboot/controller/StudentController.java
@@ -80,9 +80,19 @@ public class StudentController {
      */
     @PutMapping("students/{id}/update")
     public Student updateStudent(@RequestBody Student student,
-                                 @PathVariable("id") int studentid) {
+                                 @PathVariable("id") int studentId) {
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
         return student;
+    }
+
+    /**
+     * Spring Boot REST API that handles HTTP DELETE Request
+     * http://[::1]:8080/students/1/delete
+     */
+    @DeleteMapping("students/{id}/delete")
+    public String deleteStudent(@PathVariable("id") int studentId) {
+        System.out.println(studentId);
+        return "Student deleted successfully!";
     }
 }
