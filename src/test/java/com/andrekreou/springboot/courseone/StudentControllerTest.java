7ab8cfb640e78b413a64f1501ac93bb28c1cd3fe
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2023-02-13 21:43:21 +0200

    Added test for StudentController getStudents

--- src/test/java/com/andrekreou/springboot/courseone/StudentControllerTest.java
+++ src/test/java/com/andrekreou/springboot/courseone/StudentControllerTest.java
@@ -21,12 +21,24 @@ public class StudentControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("Should return student with Id1 and name Andreas Kreouzos")
+    @DisplayName("Should return the student with the corresponding value")
     public void getStudent() throws Exception {
         mockMvc.perform(get("/students/student"))
                 .andExpect(status().isOk())
                 .andExpect(header().string("custom-header", "andreas"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json("{\"id\":1,\"firstName\":\"Andreas\",\"lastName\":\"Kreouzos\"}"));
+    }
+
+    @Test
+    @DisplayName("Should return the list of students with the corresponding values")
+    public void getStudents() throws Exception {
+        mockMvc.perform(get("/students"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("[{\"id\":1,\"firstName\":\"Andreas\",\"lastName\":\"Kreouzos\"}," +
+                        "{\"id\":2,\"firstName\":\"Anastasios\",\"lastName\":\"Plataras\"}," +
+                        "{\"id\":3,\"firstName\":\"Antonios\",\"lastName\":\"Lazos\"}," +
+                        "{\"id\":4,\"firstName\":\"Georgios Ioannis\",\"lastName\":\"Zacharopoulos\"}]"));
     }
 }
