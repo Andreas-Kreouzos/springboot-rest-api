0ea27929cfbd9b26dc6a8579dd9a4d407ba9bfc7
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2023-02-25 13:30:48 +0200

    Refactored updateUser as UserDto

--- src/main/java/com/andrekreou/springboot/coursetwo/service/impl/UserServiceImpl.java
+++ src/main/java/com/andrekreou/springboot/coursetwo/service/impl/UserServiceImpl.java
@@ -44,11 +44,13 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User updateUser(User user) {
+    public UserDto updateUser(UserDto user) {
         User existingUser = userRepository.findById(user.getId()).get();
+        existingUser.setFirstName(user.getFirstName());
         existingUser.setLastName(user.getLastName());
         existingUser.setEmail(user.getEmail());
-        return userRepository.save(existingUser);
+        User updatedUser = userRepository.save(existingUser);
+        return UserMapper.mapToUserDto(updatedUser);
     }
 
     @Override
